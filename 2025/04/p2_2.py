b1cb19d70c1f8037432259ba04355f5d4913c49a
cefqrn <cefqrn@gmail.com>
2025-11-26 23:42:49 -0500

    04

---
+++ 2025/04/p2_2.py
@@ -0,0 +1,34 @@
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, deque, defaultdict
+from functools import cache, partial
+from itertools import chain, cycle, islice, pairwise, combinations, product
+from heapq import heappop, heappush
+from math import inf
+from re import findall
+
+directions = (0, 1), (1, 0), (0, -1), (-1, 0)
+
+from fractions import Fraction
+
+def parse(s):
+    return tuple(map(int, s.splitlines()))
+
+def solve1(data, first):
+    curr = Fraction(first)
+    for a, b in pairwise(data):
+        curr *= Fraction(a, b)
+    return int(curr)
+
+def solve(data):
+    curr = Fraction(10000000000000)
+    for a, b in pairwise(data[::-1]):
+        curr *= Fraction(a, b)
+
+    return curr.__ceil__()
+
+if __name__ == "__main__":
+    with open("everybody_codes_e2025_q04_p2.txt") as f:
+        data = parse(f.read().strip())
+
+    print(solve(data))
---
+++ 2025/04/p3.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from collections import Counter, deque, defaultdict
+from functools import cache, partial
+from itertools import chain, cycle, islice, pairwise, combinations, product
+from heapq import heappop, heappush
+from math import inf
+from re import findall
+
+directions = (0, 1), (1, 0), (0, -1), (-1, 0)
+
+from fractions import Fraction
+
+def parse(s):
+    result = []
+    for l in s.splitlines():
+        a, *b = map(int, l.split("|"))
+        if b:
+            b, = b
+        else:
+            b = a
+
+        result.append((a, b))
+    
+    return result
+
+def solve(data):
+    curr = Fraction(100)
+    for (_, a), (b, _) in pairwise(data):
+        curr *= Fraction(a, b)
+    return int(curr)
+
+if __name__ == "__main__":
+    with open("everybody_codes_e2025_q04_p3.txt") as f:
+        data = parse(f.read().strip())
+
+    print(solve(data))
